8996065dd83c4d053f3e6054672c455ca8bc0980
YashwanthRaj <yashwanthraj6383@gmail.com>
2024-07-11 10:51:43 -0400

    Part 2 - ML Integration
    This version is able to connect with the ML model and calculate the sleep time required with respect to the coffee, wakeup time and desired amount of sleep

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -5,29 +5,70 @@
 //  Created by Yashwanth Raj Varadharajan on 7/9/24.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepCount = 8.0
-    
-    // Swift UI has a separate datatype for date called Date
     @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     
     var body: some View {
-        VStack {
-            Stepper("\(sleepCount.formatted()) hours:", value: $sleepCount, in: 4...12, step: 0.25)
-            
-            DatePicker("Please enter a date:", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
-            
-            DatePicker("Please enter a date:", selection: $wakeUp, in: Date.now... ).labelsHidden()
-            
-        }.padding()
-        
+        NavigationStack{
+            VStack{
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents:.hourAndMinute).labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours: ", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily cup of coffee")
+                    .font(.headline)
+                
+                Stepper("\(coffeAmount.formatted()) cup(s): ", value: $coffeAmount, in: 1...20)
+                
+            }.navigationTitle("BetterRest")
+            //.padding()
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
+        }
     }
     
-    func exampleDates() {
-        let tommorrow = Date.now.addingTimeInterval(86400)   // 86400 is the number of hours on a day
-        let range = Date.now...tommorrow
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.hour ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem in calculating your bedtime."
+        }
+        showingAlert = true
     }
 }
 
